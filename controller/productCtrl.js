030c0445c0bb6af17ce9e8a8fc803905f5f704cd
Ajeet Kumar Jaiswal <ajeet.jaiswal@capriglobal.in>
2024-03-24 03:55:51 +0530

    added api field limiting, sorting, filtering and paginating

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -19,7 +19,7 @@ const updateProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
   try {
     if (req.body.title) {
-        req.body.slug = slugify(req.body.title);
+      req.body.slug = slugify(req.body.title);
     }
     const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
       new: true,
@@ -33,7 +33,48 @@ const updateProduct = asyncHandler(async (req, res) => {
 // fetch All products
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
-    const allProduct = await Product.find();
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((ele) => delete queryObj[ele]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // Pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) {
+        throw new Error("This Page does not exists");
+      }
+    }
+
+    const allProduct = await query;
     res.json(allProduct);
   } catch (error) {
     throw new Error(error);
@@ -53,16 +94,22 @@ const getAProduct = asyncHandler(async (req, res) => {
 
 // delete a product
 const deleteAProduct = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    try {
-        const product = await Product.findByIdAndDelete(id)
-        if (!product) {
-            res.status(404).send({ message: "Product Does not Exists"});
-        }
-        res.json({message:"Product Deleted Successfully!"})
-    } catch (error) {
-        throw new Error(error)
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).send({ message: "Product Does not Exists" });
     }
+    res.json({ message: "Product Deleted Successfully!" });
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
-module.exports = { createProduct, getAllProduct, getAProduct, updateProduct, deleteAProduct };
+module.exports = {
+  createProduct,
+  getAllProduct,
+  getAProduct,
+  updateProduct,
+  deleteAProduct,
+};
--- models/productModel.js
+++ models/productModel.js
@@ -37,6 +37,7 @@ var productSchema = new mongoose.Schema(
     sold: {
       type: Number,
       default: 0,
+      // select: false --> to hide in response
     },
     images: {
       type: Array,
